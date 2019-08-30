9b58523af9e412c0c2d0aaee6d2b4eb7c62df9f6
jaredgorski <jared.gorski@liferay.cloud>
2019-08-29 21:57:07 -0700

    Initial commit

---
+++ src/util/log.rs
@@ -0,0 +1,23 @@
+pub fn error(err: &str) {
+    let error: String = format!("error: {}", err);
+    println!("{}", error);
+}
+
+pub fn usage(bin: &str, arg_doc: Option<&Vec<String>>) {
+    let mut usage: String = String::from("\nUSAGE:");
+
+    let doc_from_str = |bin: &str, doc: &str| -> String {
+        return format!("\n    {} {}", bin, doc);
+    };
+
+    if let Some(doc) = arg_doc {
+        for i in 1..doc.len() {
+            usage += doc_from_str(bin, &doc[i]).as_str();
+        }
+    } else {
+        let default_doc: &str = "[-p | -r | MODE] [OPTIONS]";
+        usage += doc_from_str(bin, default_doc).as_str();
+    }
+
+    println!("{}\n", usage);
+}
