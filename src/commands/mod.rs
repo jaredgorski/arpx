f4b4805267404e7ae0245dece5286860ca9498c1
jaredgorski <jared.gorski@liferay.cloud>
2019-11-30 15:24:50 -0800

    Bump to 0.0.2, remove unnecessary command logic

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -7,44 +7,30 @@ pub mod run;
 pub struct Command {
     pub profile: Profile,
     pub daemon_mode: bool,
-    pub pid_to_kill: String,
     pub processes_to_run: Vec<String>,
     pub verbosity: String,
 }
 
 pub fn get_command(matches: ArgMatches) -> Command {
-    let mut cmd_profile: Profile = Profile::new();
-    let mut cmd_daemon: bool = false;
-    let mut cmd_kill: String = String::new();
+    let file: String = matches.value_of("file").unwrap_or("arpx.yaml").to_string();
+    let cmd_profile = match get_profile(file) {
+        Ok(profile) => profile,
+        Err(error) => panic!(error),
+    };
+
     let mut cmd_processes: Vec<String> = Vec::new();
-
-    if matches.is_present("ls") {
-        println!("NOTE: Processes cannot be listed. Feature not fully implemented yet.");
+    if let Some(ref process) = matches.value_of("process") {
+        cmd_processes.push(process.to_string());
     } else {
-        let file: String = matches.value_of("file").unwrap_or("arpx.yaml").to_string();
-
-        cmd_profile = match get_profile(file) {
-            Ok(profile) => profile,
-            Err(error) => panic!(error),
-        };
-
-        if matches.is_present("daemon") {
-            cmd_daemon = true;
-            println!("NOTE: Daemon mode not invoked. Feature not fully implemented yet.");
+        for process in cmd_profile.processes.iter() {
+            cmd_processes.push(process.name[..].to_string());
         }
+    }
 
-        if let Some(pid) = matches.value_of("kill") {
-            cmd_kill = pid.to_string();
-            println!("NOTE: Process cannot be killed. Feature not fully implemented yet.");
-        }
-
-        if let Some(ref process) = matches.value_of("process") {
-            cmd_processes.push(process.to_string());
-        } else {
-            for process in cmd_profile.processes.iter() {
-                cmd_processes.push(process.name[..].to_string());
-            }
-        }
+    let mut cmd_daemon: bool = false;
+    if matches.is_present("daemon") {
+        cmd_daemon = true;
+        println!("NOTE: Daemon mode not invoked. Feature not fully implemented yet.");
     }
 
     let cmd_verbosity = match matches.occurrences_of("v") {
@@ -61,7 +47,6 @@ pub fn get_command(matches: ArgMatches) -> Command {
     Command {
         profile: cmd_profile,
         daemon_mode: cmd_daemon,
-        pid_to_kill: cmd_kill,
         processes_to_run: cmd_processes,
         verbosity: cmd_verbosity,
     }
