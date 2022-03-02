64c8e322845d9f76f47a2dd3f66e3882c075463a
jaredgorski <jared.gorski@liferay.cloud>
2022-03-01 23:32:06 -0800

    Move logs util to src, remove error file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 #![forbid(unsafe_code)]
 
 mod cli;
+mod logs;
 mod runtime;
-mod util;
 
 use anyhow::{Context, Result};
 use cli::Cli;
 use log::{debug, LevelFilter};
+use logs::Logs;
 use runtime::{local_bin::BinCommand, Runtime};
-use util::logs::Logs;
 
 fn main() -> Result<()> {
     let matches = Cli::run();
