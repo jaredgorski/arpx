80ffa627df0fda52570f4df6832d6c27d2bf08c7
jaredgorski <jared.gorski@liferay.cloud>
2022-03-10 00:01:37 -0800

    Add WIP windows support

--- tests/runtime_win.rs
+++ tests/runtime_win.rs
@@ -2,7 +2,7 @@ mod common;
 
 use common::TC;
 
-#[cfg(unix)]
+#[cfg(windows)]
 test!(single_job_with_single_task, |t: TC| {
     let (out, err) = t
         .profile(
@@ -12,25 +12,20 @@ test!(single_job_with_single_task, |t: TC| {
 
             processes:
                 p1:
-                    command: |
-                        echo foo
-                        sleep 0.1
-                        echo bar
-                        sleep 0.1
-                        echo baz
-                        sleep 0.1
+                    command: 'echo foo & timeout /t 1 >nul & echo bar & timeout /t 1 >nul & echo baz & timeout /t 1 >nul'
         "#,
         )
         .opts("-j test")
         .run()
         .unwrap();
 
-    assert_eq!(vec!["[p1] foo", "[p1] bar", "[p1] baz"], out[1..4]);
+    debug!(out);
+    assert_eq!(vec!["[p1] foo \r", "[p1] bar \r", "[p1] baz \r"], out[1..4]);
     assert_eq!(5, out.len());
     assert_eq!(0, err.len());
 });
 
-#[cfg(unix)]
+#[cfg(windows)]
 test!(single_job_with_multiple_tasks, |t: TC| {
     let (out, err) = t
         .profile(
@@ -42,28 +37,23 @@ test!(single_job_with_multiple_tasks, |t: TC| {
 
             processes:
                 p1:
-                    command: |
-                        echo foo
-                        sleep 0.1
+                    command: 'echo foo & timeout /t 1 >nul'
                 p2:
-                    command: |
-                        echo bar
-                        sleep 0.1
-                        echo baz
-                        sleep 0.1
+                    command: 'echo bar & timeout /t 1 >nul & echo baz & timeout /t 1 >nul'
         "#,
         )
         .opts("-j test")
         .run()
         .unwrap();
 
-    assert_eq!("[p1] foo", out[1]);
-    assert_eq!(vec!["[p2] bar", "[p2] baz"], out[4..6]);
+    debug!(out);
+    assert_eq!("[p1] foo \r", out[1]);
+    assert_eq!(vec!["[p2] bar \r", "[p2] baz \r"], out[4..6]);
     assert_eq!(7, out.len());
     assert_eq!(0, err.len());
 });
 
-#[cfg(unix)]
+#[cfg(windows)]
 test!(single_job_with_concurrent_task, |t: TC| {
     let (out, err) = t
         .profile(
@@ -77,21 +67,16 @@ test!(single_job_with_concurrent_task, |t: TC| {
 
             processes:
                 p1:
-                    command: |
-                        echo foo
-                        sleep 0.2
-                        echo baz
+                    command: 'echo foo & timeout /t 2 >nul & echo baz'
                 p2:
-                    command: |
-                        sleep 0.1
-                        echo bar
-                        sleep 0.1
+                    command: 'timeout /t 1 >nul & echo bar & timeout /t 1 >nul'
         "#,
         )
         .opts("-j test")
         .run()
         .unwrap();
 
+    debug!(out);
     assert_btw!("foo", out, 0, 3);
     assert_btw!("bar", out, 2, 4);
     assert_btw!("baz", out, 3, 6);
@@ -99,7 +84,7 @@ test!(single_job_with_concurrent_task, |t: TC| {
     assert_eq!(0, err.len());
 });
 
-#[cfg(unix)]
+#[cfg(windows)]
 test!(single_job_with_single_and_concurrent_task, |t: TC| {
     let (out, err) = t
         .profile(
@@ -114,26 +99,19 @@ test!(single_job_with_single_and_concurrent_task, |t: TC| {
 
             processes:
                 p0:
-                    command: |
-                        echo qux
-                        sleep 0.1
+                    command: 'echo qux & timeout /t 1 >nul'
                 p1:
-                    command: |
-                        echo foo
-                        sleep 0.2
-                        echo baz
+                    command: 'echo foo & timeout /t 2 >nul & echo baz'
                 p2:
-                    command: |
-                        sleep 0.1
-                        echo bar
-                        sleep 0.1
+                    command: 'timeout /t 1 >nul & echo bar & timeout /t 1 >nul'
         "#,
         )
         .opts("-j test")
         .run()
         .unwrap();
 
-    assert_eq!("[p0] qux", out[1]);
+    debug!(out);
+    assert_eq!("[p0] qux \r", out[1]);
     assert_btw!("foo", out, 2, 6);
     assert_btw!("bar", out, 3, 9);
     assert_btw!("baz", out, 6, 9);
@@ -141,7 +119,7 @@ test!(single_job_with_single_and_concurrent_task, |t: TC| {
     assert_eq!(0, err.len());
 });
 
-#[cfg(unix)]
+#[cfg(windows)]
 test!(multiple_jobs, |t: TC| {
     let (out, err) = t
         .profile(
@@ -152,28 +130,23 @@ test!(multiple_jobs, |t: TC| {
 
             processes:
                 p1:
-                    command: |
-                        echo foo
-                        sleep 0.1
+                    command: 'echo foo & timeout /t 1 >nul'
                 p2:
-                    command: |
-                        echo bar
-                        sleep 0.1
-                        echo baz
-                        sleep 0.1
+                    command: 'echo bar & timeout /t 1 >nul & echo baz & timeout /t 1 >nul'
         "#,
         )
         .opts("-j test_1 -j test_2")
         .run()
         .unwrap();
 
-    assert_eq!("[p1] foo", out[1]);
-    assert_eq!(vec!["[p2] bar", "[p2] baz"], out[4..6]);
+    debug!(out);
+    assert_eq!("[p1] foo \r", out[1]);
+    assert_eq!(vec!["[p2] bar \r", "[p2] baz \r"], out[4..6]);
     assert_eq!(7, out.len());
     assert_eq!(0, err.len());
 });
 
-#[cfg(unix)]
+#[cfg(windows)]
 test!(job_with_onsucceed_process, |t: TC| {
     let (out, err) = t
         .profile(
@@ -183,33 +156,28 @@ test!(job_with_onsucceed_process, |t: TC| {
 
             processes:
                 one:
-                    command: |
-                        echo foo
-                        sleep 0.1
+                    command: 'echo foo & timeout /t 1 >nul'
                     onsucceed: two
                 two:
-                    command: |
-                        echo bar
-                        sleep 0.1
+                    command: 'echo bar & timeout /t 1 >nul'
                     onsucceed: three
                 three:
-                    command: |
-                        echo baz
-                        sleep 0.1
+                    command: 'echo baz & timeout /t 1 >nul'
         "#,
         )
         .opts("-j test")
         .run()
         .unwrap();
 
-    assert_eq!("[one] foo", out[1]);
+    debug!(out);
+    assert_eq!("[one] foo \r", out[1]);
     assert_btw!("bar", out, 3, 8);
     assert_btw!("baz", out, 5, 8);
     assert_eq!(9, out.len());
     assert_eq!(0, err.len());
 });
 
-#[cfg(unix)]
+#[cfg(windows)]
 test!(job_with_onfail_process, |t: TC| {
     let (out, err) = t
         .profile(
@@ -219,35 +187,28 @@ test!(job_with_onfail_process, |t: TC| {
 
             processes:
                 one:
-                    command: |
-                        echo foo
-                        sleep 0.1
-                        exit 1
+                    command: 'echo foo & timeout /t 1 >nul & exit 1'
                     onfail: two
                 two:
-                    command: |
-                        echo bar
-                        sleep 0.1
-                        exit 1
+                    command: 'echo bar & timeout /t 1 >nul & exit 1'
                     onfail: three
                 three:
-                    command: |
-                        echo baz
-                        sleep 0.1
+                    command: 'echo baz & timeout /t 1 >nul'
         "#,
         )
         .opts("-j test")
         .run()
         .unwrap();
 
-    assert_eq!("[one] foo", out[1]);
+    debug!(out);
+    assert_eq!("[one] foo \r", out[1]);
     assert_btw!("bar", out, 3, 8);
     assert_btw!("baz", out, 5, 8);
     assert_eq!(9, out.len());
     assert_eq!(0, err.len());
 });
 
-#[cfg(unix)]
+#[cfg(windows)]
 test!(job_with_single_log_monitor, |t: TC| {
     let (out, err) = t
         .profile(
@@ -257,19 +218,14 @@ test!(job_with_single_log_monitor, |t: TC| {
 
             processes:
                 p1:
-                    command: |
-                        echo foo
-                        sleep 0.1
-                        echo bar
-                        sleep 0.1
+                    command: 'echo foo & timeout /t 1 >nul & echo bar & timeout /t 1 >nul'
                 p2:
-                    command: |
-                        echo baz
-                        sleep 0.1
+                    command: 'echo baz & timeout /t 1 >nul'
             log_monitors:
                 m1:
                     buffer_size: 1
-                    test: 'echo "$ARPX_BUFFER" | grep -q "bar"'
+                    test: |
+                        echo.%ARPX_BUFFER%|findstr /C:"bar" >nul 2>&1
                     ontrigger: p2
         "#,
         )
@@ -277,13 +233,14 @@ test!(job_with_single_log_monitor, |t: TC| {
         .run()
         .unwrap();
 
-    assert_eq!(vec!["[p1] foo", "[p1] bar"], out[1..3]);
+    debug!(out);
+    assert_eq!(vec!["[p1] foo \r", "[p1] bar \r"], out[1..3]);
     assert_btw!("baz", out, 3, 6);
     assert_eq!(7, out.len());
     assert_eq!(0, err.len());
 });
 
-#[cfg(unix)]
+#[cfg(windows)]
 test!(job_with_multiple_log_monitors, |t: TC| {
     let (out, err) = t
         .profile(
@@ -293,28 +250,21 @@ test!(job_with_multiple_log_monitors, |t: TC| {
 
             processes:
                 p1:
-                    command: |
-                        echo foo
-                        sleep 0.2
-                        echo bar
-                        sleep 0.1
+                    command: 'echo foo & timeout /t 2 >nul & echo bar & timeout /t 1 >nul'
                 p2:
-                    command: |
-                        echo baz
-                        sleep 0.1
+                    command: 'echo baz & timeout /t 1 >nul'
                 p3:
-                    command: |
-                        sleep 0.3
-                        echo qux
-                        sleep 0.1
+                    command: 'timeout /t 3 >nul & echo qux & timeout /t 1 >nul'
             log_monitors:
                 m1:
                     buffer_size: 1
-                    test: 'echo "$ARPX_BUFFER" | grep -q "foo"'
+                    test: |
+                        echo.%ARPX_BUFFER%|findstr /C:"foo" >nul 2>&1
                     ontrigger: p2
                 m2:
                     buffer_size: 1
-                    test: 'echo "$ARPX_BUFFER" | grep -q "bar"'
+                    test: |
+                        echo.%ARPX_BUFFER%|findstr /C:"bar" >nul 2>&1
                     ontrigger: p3
         "#,
         )
@@ -322,7 +272,8 @@ test!(job_with_multiple_log_monitors, |t: TC| {
         .run()
         .unwrap();
 
-    assert_eq!("[p1] foo", out[1]);
+    debug!(out);
+    assert_eq!("[p1] foo \r", out[1]);
     assert_btw!("baz", out, 2, 6);
     assert_btw!("bar", out, 2, 6);
     assert_btw!("qux", out, 7, 9);
